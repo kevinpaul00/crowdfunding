b4d91c62e1223e7c5d703c875a65bcf24a6eecb9
Kevin Paul <kevinpaul1100@gmail.com>
2020-09-13 20:19:09 +0530

    Added contribute function

--- Project.sol
+++ Project.sol
@@ -8,29 +8,44 @@ contract Project{
     }
 
     //State variables 
-    address payable public creator;
+    address payable public projectOwner;
     uint256 public currentBalance;
     uint public amountGoal;
-    uint public completeAt;
-    uint public raiseBy;
+    //uint public completeAt;
+    uint public deadLine;
     string public projectTitle;
     string public projectDescription;
-    ProjectState public state = ProjectState.FundRaising;
+    ProjectState public currentState = ProjectState.FundRaising;
     mapping (address => uint) public contributions;
 
     //Constructor for the contract
     constructor(
+        address payable projectStarter,
         uint goal,
-        uint complete,
+        // uint complete,
+        uint deadline,
         string memory title,
         string memory description
     ){
+        projectOwner = projectStarter;
         amountGoal = goal;
-        completeAt = complete;
+        //completeAt = complete;
+        deadLine = deadline;
         projectTitle = title;
         projectDescription = description;
+        currentBalance = 0;
     }
 
-    
+    modifier verifyProjectState(ProjectState _state){
+        require(currentState == _state);
+        _;
+    }
+
+    function contribute() external verifyProjectState(ProjectState.FundRaising) payable {
+        require (msg.sender != projectOwner);
+        contributions[msg.sender] = contributions[msg.sender] + (msg.value);
+        currentBalance = currentBalance + (msg.value);
+    }
+
 }
 
