08cb5f6a300a178d06939ac4222cb134378af9d2
Kevin Paul <kevinpaul1100@gmail.com>
2020-09-17 19:58:25 +0530

    project contract updated

--- Project.sol
+++ Project.sol
@@ -36,6 +36,9 @@ contract Project{
         currentBalance = 0;
     }
 
+    event FundingReceived(address contributor, uint amount, uint currentTotal);
+    event ProjectOwnerPaid(address prOwner);
+
     modifier verifyProjectState(ProjectState _state){
         require(currentState == _state);
         _;
@@ -45,7 +48,66 @@ contract Project{
         require (msg.sender != projectOwner);
         contributions[msg.sender] = contributions[msg.sender] + (msg.value);
         currentBalance = currentBalance + (msg.value);
+        emit FundingReceived(msg.sender, msg.value, currentBalance);
+        checkFundingState();
     }
 
+    function checkFundingState() public{
+        if(currentBalance >= amountGoal){
+            currentState = ProjectState.successful;
+            payProjectOwner();
+        }
+        else if(block.timestamp > deadLine){
+            currentState = ProjectState.expired;
+        }
+    }
+
+    function payProjectOwner() internal verifyProjectState(ProjectState.successful) returns(bool){
+        uint256 raisedAmount = currentBalance;
+        currentBalance = 0;
+
+        if(projectOwner.send(raisedAmount)){
+            emit ProjectOwnerPaid(projectOwner);
+            return true;
+        }
+        else{
+            currentBalance = raisedAmount;
+            currentState = ProjectState.successful;
+            return false;
+        }
+    }
+
+    function getRefund() public verifyProjectState(ProjectState.expired) returns(bool){
+        require(contributions[msg.sender]>0);
+        
+        uint refundAmount = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+
+        if(msg.sender.send(refundAmount)){
+            currentBalance = currentBalance - refundAmount;
+        }
+        else{  
+            contributions[msg.sender] = refundAmount;
+            return false;   
+        }
+    }
+
+    function getProjectDetails() public view returns(
+        address projOwner,
+        string memory title,
+        string memory description,
+        uint amtGoal,
+        uint currBalance,
+        uint deadline,
+        ProjectState currState
+    ){
+        projOwner = projectOwner;
+        title = projectTitle;
+        description = projectDescription;
+        amtGoal = amountGoal;
+        currBalance = currentBalance;
+        deadline = deadLine;
+        currState = currentState;
+    }
 }
 
