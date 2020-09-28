c14d881fc42d66d9840609c7aaeafcbe108cf7bd
Kevin Paul <kevinpaul1100@gmail.com>
2020-09-28 17:49:19 +0530

    .

--- Contracts/Funding.sol
+++ Contracts/Funding.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.7.0;
 
-//import "./Project.sol";
+import "./Project.sol";
 
 contract Funding{
     Project[] private projects;
@@ -12,6 +12,9 @@ contract Funding{
         uint256 deadline,
         uint256 goalAmount
     );
+
+
+    constructor(){}
     
     function startProject(
         string calldata title,
@@ -36,116 +39,5 @@ contract Funding{
 
     function returnAllProjects() external view returns(Project[] memory){
         return projects;
-    }
-}
-
-contract Project{
-
-    //Project state enumeration
-    enum ProjectState{
-        FundRaising, expired, successful
-    }
-
-    //State variables 
-    address payable public projectOwner;
-    uint256 public currentBalance;
-    uint public amountGoal;
-    //uint public completeAt;
-    uint public deadLine;
-    string public projectTitle;
-    string public projectDescription;
-    ProjectState public currentState = ProjectState.FundRaising;
-    mapping (address => uint) public contributions;
-
-    //Constructor for the contract
-    constructor(
-        address payable projectStarter,
-        uint goal,
-        // uint complete,
-        uint deadline,
-        string memory title,
-        string memory description
-    ){
-        projectOwner = projectStarter;
-        amountGoal = goal;
-        //completeAt = complete;
-        deadLine = deadline;
-        projectTitle = title;
-        projectDescription = description;
-        currentBalance = 0;
-    }
-
-    event FundingReceived(address contributor, uint amount, uint currentTotal);
-    event ProjectOwnerPaid(address prOwner);
-
-    modifier verifyProjectState(ProjectState _state){
-        require(currentState == _state);
-        _;
-    }
-
-    function contribute() external verifyProjectState(ProjectState.FundRaising) payable {
-        require (msg.sender != projectOwner);
-        contributions[msg.sender] = contributions[msg.sender] + (msg.value);
-        currentBalance = currentBalance + (msg.value);
-        emit FundingReceived(msg.sender, msg.value, currentBalance);
-        checkFundingState();
-    }
-
-    function checkFundingState() public{
-        if(currentBalance >= amountGoal){
-            currentState = ProjectState.successful;
-            payProjectOwner();
-        }
-        else if(block.timestamp > deadLine){
-            currentState = ProjectState.expired;
-        }
-    }
-
-    function payProjectOwner() internal verifyProjectState(ProjectState.successful) returns(bool){
-        uint256 raisedAmount = currentBalance;
-        currentBalance = 0;
-
-        if(projectOwner.send(raisedAmount)){
-            emit ProjectOwnerPaid(projectOwner);
-            return true;
-        }
-        else{
-            currentBalance = raisedAmount;
-            currentState = ProjectState.successful;
-            return false;
-        }
-    }
-
-    function getRefund() public verifyProjectState(ProjectState.expired) returns(bool){
-        require(contributions[msg.sender]>0);
-        
-        uint refundAmount = contributions[msg.sender];
-        contributions[msg.sender] = 0;
-
-        if(msg.sender.send(refundAmount)){
-            currentBalance = currentBalance - refundAmount;
-        }
-        else{  
-            contributions[msg.sender] = refundAmount;
-            return false;   
-        }
-    }
-
-    function getProjectDetails() public view returns(
-        address projOwner,
-        string memory title,
-        string memory description,
-        uint amtGoal,
-        uint currBalance,
-        uint deadline,
-        ProjectState currState
-    ){
-        projOwner = projectOwner;
-        title = projectTitle;
-        description = projectDescription;
-        amtGoal = amountGoal;
-        currBalance = currentBalance;
-        deadline = deadLine;
-        currState = currentState;
     }
 }
--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ def deployContract(web3, contractInterface):
     amount = 300
     txHash = web3.eth.contract(
         abi = contractInterface['abi'],
-        bytecode = contractInterface['bin']).constructor(starterAddress, amount, days, title, description).transact()
+        bytecode = contractInterface['bin']).constructor().transact()
 
     address = web3.eth.getTransactionReceipt(txHash)['contractAddress']
     return address
@@ -24,59 +24,11 @@ def deployContract(web3, contractInterface):
 ganacheURL = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganacheURL))
 
-fundingContractPath= "../Contracts/Funding.sol"
+fundingContractPath= "../Contracts/Crowdfunding.sol"
 compiledSol = compileSourceFile(fundingContractPath)
-
 
 contractId, contractInterface = compiledSol.popitem()
 
 fundingContractAddress = deployContract(web3, contractInterface)
 
 print(f'Deployed {contractId} to: {fundingContractAddress}\n')
-
-# print(f'Deployed {contract_id} to: {address}\n')
-
-# store_var_contract = w3.eth.contract(address=address, abi=contract_interface["abi"])
-
-# gas_estimate = store_var_contract.functions.setVar(255).estimateGas()
-# print(f'Gas estimate to transact with setVar: {gas_estimate}')
-
-# if gas_estimate < 100000:
-#      print("Sending transaction to setVar(255)\n")
-#      tx_hash = store_var_contract.functions.setVar(255).transact()
-#      receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-#      print("Transaction receipt mined:")
-#      pprint.pprint(dict(receipt))
-#      print("\nWas transaction successful?")
-#      pprint.pprint(receipt["status"])
-# else:
-#      print("Gas cost exceeds 100000")
-
-
-
-
-
-
-
-
-
-
-# Original file 
-
-# with open("./abi.json") as f:
-#     abi = json.load(f)
-
-# address = "0x2cAaaf7af99eE2c5F965B11060a85465f277B88f"
-# address = web3.toChecksumAddress(address)
-
-# contract = web3.eth.contract(address = address, abi = abi)
-
-# print(contract.functions.sampleGreeting().call())
-
-# title = "Medivisor"
-# description = "Rural HealthCare"
-# days = 1
-# amount = 300
-
-#contract.functions.startProject(title, description, days, amount).transact()
-
