348394ab071d2796c9dcb19623813051983b7fa1
Kevin Paul <kevinpaul1100@gmail.com>
2020-10-19 22:11:10 +0530

    TimeOut

--- client/src/Components/DisplayProject.js
+++ client/src/Components/DisplayProject.js
@@ -52,7 +52,6 @@ class DisplayProject extends Component {
             });
 
             setTimeout(() => {
-                
                 this.setState({
                     projects: p,
                     isLoading: false
